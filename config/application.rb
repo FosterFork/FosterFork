0526bb9183c4613e2b1d3cedb8a958142f3abb00
Ernesto Caspar <casparnesto@gmail.com>
2016-04-03 12:56:09 +0200

    application: refuse startup without secret key

--- config/application.rb
+++ config/application.rb
@@ -32,5 +32,7 @@ module FosterFork
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    raise "No secret key set in the settings file. Aborting." if Settings.secret_key.blank?
   end
 end
