fd8adec2031359139e609701f37e8a7c680858eb
Ernesto Caspar <casparnesto@gmail.com>
2016-04-07 16:41:44 +0200

    map view: do not pass project.id

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,12 +3,12 @@ class PagesController < ApplicationController
   def map
     projects = Project.publicly_visible
     selected = params[:select].to_i rescue nil
-    @map_data = projects.map do |i|
+    @map_data = projects.map do |project|
       {
-        x: CGI.escape(i.latitude.to_s),
-        y: CGI.escape(i.longitude.to_s),
-        url: project_popup_content_url(i, id: i.id),
-        open: selected == i.id
+        x: CGI.escape(project.latitude.to_s),
+        y: CGI.escape(project.longitude.to_s),
+        url: project_popup_content_url(project),
+        open: selected == project.id
       }
     end
   end
