6db553a6ae870c7e8de0813a545db13c831c1380
Ernesto Caspar <casparnesto@gmail.com>
2016-04-11 11:05:50 +0200

    fix project comments job

--- app/jobs/send_project_comments_job.rb
+++ app/jobs/send_project_comments_job.rb
@@ -3,7 +3,8 @@ class SendProjectCommentsJob
 
   def perform(project, comment)
     project.participations.each do |participation|
-      ProjectMailer.message_mail(comment, participation).deliver_now
+      next if comment.user == participation.user
+      ProjectMailer.comment_mail(comment, participation).deliver_now
       user = participation.user
       Rails.logger.info "Message ##{comment.id} ('#{comment.message.title}') delivered to #{user.name} <#{user.email}>"
     end
