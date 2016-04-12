453c937c7ef7af1ae9acfcdc446b21d0e74f99be
Ernesto Caspar <casparnesto@gmail.com>
2016-04-12 18:33:32 +0200

    don't convert symbols to hashes

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -1,5 +1,6 @@
 [:smtp_settings, :delivery_method, :default_url_options].each do |key|
   if Settings.mail[key]
-    ActionMailer::Base.send(:"#{key}=", Settings.mail[key].to_hash)
+    ActionMailer::Base.send(:"#{key}=",
+      Settings.mail[key].is_a?(Symbol) ? Settings.mail[key] : Settings.mail[key].to_hash)
   end
 end
