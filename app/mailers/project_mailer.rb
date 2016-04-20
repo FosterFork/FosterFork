b5743b1792eda7af5da1883027a586290562a743
Ernesto Caspar <casparnesto@gmail.com>
2016-04-20 22:36:42 +0200

    project mailer: set correct locales

--- app/mailers/project_mailer.rb
+++ app/mailers/project_mailer.rb
@@ -3,7 +3,7 @@ class ProjectMailer < ApplicationMailer
   def approved_mail(project)
     @project = project
     subject = I18n.t('mailer.approved.subject', project_title: @project.title)
-    mail(to: @project.owner.email, subject: subject)
+    mail(to: @project.owner.email, subject: subject, locale: @project.owner.locale)
   end
 
   def message_mail(message, participation)
@@ -16,7 +16,7 @@ class ProjectMailer < ApplicationMailer
                      project_title: @project.title,
                      message_title: @message.title)
 
-    mail(to: @user.email, subject: subject)
+    mail(to: @user.email, subject: subject, locale: @user.locale)
   end
 
   def comment_mail(comment, participation)
@@ -30,13 +30,13 @@ class ProjectMailer < ApplicationMailer
                      message_title: @comment.message.title,
                      author: @comment.user.name)
 
-    mail(to: @user.email, subject: subject)
+    mail(to: @user.email, subject: subject, locale: @user.locale)
   end
 
   def new_project_near_you_mail(project, user)
     @project, @user = project, user
     subject = I18n.t('mailer.new_project_near_you.subject', project_title: @project.title)
-    mail(to: @user.email, subject: subject)
+    mail(to: @user.email, subject: subject, locale: @user.locale)
   end
 
 end
