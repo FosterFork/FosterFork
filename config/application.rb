80b53f34a2963a4821deea16718c9d46d5073a77
Ernesto Caspar <casparnesto@gmail.com>
2016-04-20 22:06:20 +0200

    config: set 1st configured locale as default

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,7 @@ module FosterFork
 
     if Settings.locales&.any?
       config.i18n.available_locales = I18n.available_locales & Settings.locales.keys.map(&:to_sym) rescue []
+      config.i18n.default_locale = Settings.locales.keys.first
     end
 
     if Settings.paperclip_options?
@@ -32,7 +33,6 @@ module FosterFork
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    #config.i18n.default_locale = config.i18n.available_locales.first
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
