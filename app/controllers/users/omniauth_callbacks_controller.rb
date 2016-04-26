41b6ffbe803b20434cdc70173d86d2794db10b5b
Ernesto Caspar <casparnesto@gmail.com>
2016-04-26 15:04:40 +0200

    fix omniauth_callbacks_controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -8,7 +8,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     user = User.from_omniauth(request.env["omniauth.auth"])
 
     if user.persisted?
-      sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
+      sign_in_and_redirect user, event: :authentication # this will throw if @user is not activated
       set_flash_message(:notice, :success, kind: get_omniauth_setting_for(provider, :pretty_name)) if is_navigational_format?
     else
       session["devise.user_attributes"] = user.attributes
