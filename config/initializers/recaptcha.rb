98429a03f1a5f9ca2e5726e781beed39cba943b8
Ernesto Caspar <casparnesto@gmail.com>
2016-06-06 12:29:15 +0200

    recaptcha: drop deprecated config key

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -2,6 +2,5 @@ if Settings.recaptcha
   Recaptcha.configure do |config|
     config.public_key  = Settings.recaptcha.site_key
     config.private_key = Settings.recaptcha.secret_key
-    config.use_ssl_by_default = true
   end
 end
