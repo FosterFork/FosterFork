3ae2756c0c040ca35794fa0fb44cf9921b715c41
Ernesto Caspar <casparnesto@gmail.com>
2016-06-15 12:09:39 +0200

    cover new root page in feature specs

--- spec/features/pages_spec.rb
+++ spec/features/pages_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
+feature 'View the root page', type: :feature do
+  scenario 'user sees relevant page title' do
+    visit root_path
+    expect(page).to have_title(Settings.platform_name)
+  end
+end
+
 feature 'View the map', type: :feature do
   scenario 'user sees relevant page title' do
     visit map_path
