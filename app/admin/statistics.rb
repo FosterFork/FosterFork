ec2d49154ee58c8e66c690133dc24eae30357822
Ernesto Caspar <casparnesto@gmail.com>
2016-06-16 19:39:16 +0200

    cleanup statistic code

--- app/admin/statistics.rb
+++ app/admin/statistics.rb
@@ -4,15 +4,11 @@ ActiveAdmin.register_page "Statistic plots" do
   menu label: "Statistic plots"
 
   content title: "Statistic plots" do
-    [
-      { data: :confirmed_users,           label: "Confirmed users" },
-      { data: :projects,                  label: "Projects" },
-      { data: :publicly_visible_projects, label: "Publicy visible projects" },
-    ].each do |p|
+    Statistic.data_for_plot.each do |p|
       columns do
         column do
           panel p[:label] do
-            div class: "statistic-plot", style: "height: 300px; width: 100%;", "data-statistics": Statistic.json_for_plot(p[:data])
+            div class: "statistic-plot", style: "height: 300px; width: 100%;", "data-statistics": p[:data]
           end
         end
       end
--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -2,32 +2,43 @@ class Statistic < ActiveRecord::Base
   validates_presence_of :data
   serialize :data
 
-  def self.snapshot
-    create!(data: {
-      users: User.count,
-      confirmed_users: User.where.not(confirmed_at: nil).count,
-      projects: Project.count,
-      publicly_visible_projects: Project.publicly_visible.count,
-      unresolved_abuse_reports: AbuseReport.where(resolver: nil).count,
-      resolved_abuse_reports: AbuseReport.where.not(resolver: nil).count,
-      messages: Message.count,
-      comments: Comment.count,
-    })
-  end
-
-  def self.json_for_plot(key = nil)
-    data = all.map do |s|
-      d = { date: s.created_at.to_i * 1000 }
-
-      if key
-        d[key] = s.data[key]
-      else
-        d.merge! s.data
-      end
-
-      d
+  class << self
+    def snapshot
+      create!(data: {
+        users: User.count,
+        confirmed_users: User.where.not(confirmed_at: nil).count,
+        projects: Project.count,
+        publicly_visible_projects: Project.publicly_visible.count,
+        unresolved_abuse_reports: AbuseReport.where(resolver: nil).count,
+        resolved_abuse_reports: AbuseReport.where.not(resolver: nil).count,
+        messages: Message.count,
+        comments: Comment.count,
+      })
     end
 
-    data.to_json
+    def json_for_plot(key = nil)
+      data = all.map do |s|
+        d = { date: s.created_at.to_i * 1000 }
+
+        if key
+          d[key] = s.data[key]
+        else
+          d.merge! s.data
+        end
+
+        d
+      end
+
+      data.to_json
+    end
+
+    def data_for_plot
+      [
+        { data: json_for_plot(:confirmed_users),           label: "Confirmed users" },
+        { data: json_for_plot(:projects),                  label: "Projects" },
+        { data: json_for_plot(:publicly_visible_projects), label: "Publicy visible projects" },
+      ]
+    end
   end
+
 end
