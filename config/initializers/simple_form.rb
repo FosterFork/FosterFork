d78b98227e7709f488bbd2368c53fa6f3e4bd5d1
Ernesto Caspar <casparnesto@gmail.com>
2016-06-19 21:47:49 +0200

    SimpleForm: disable browser validations
    Test show the HTML5 browser form validations do more harm than they help,
    because they do not highlight the missing information sufficiently.
    
    Disable them for now.

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -140,6 +140,7 @@ SimpleForm.setup do |config|
   # Check the Bootstrap docs (http://getbootstrap.com)
   # to learn about the different styles for forms and inputs,
   # buttons and other elements.
+  config.browser_validations = false
   config.default_wrapper = :vertical_form
   config.wrapper_mappings = {
     check_boxes: :vertical_radio_and_checkboxes,
