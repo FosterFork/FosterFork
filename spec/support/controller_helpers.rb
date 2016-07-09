a11901083ec459bd660c6f8a2b4b46bbe7dd3236
Ernesto Caspar <casparnesto@gmail.com>
2016-07-09 19:01:05 +0200

    bundle update

--- spec/support/controller_helpers.rb
+++ spec/support/controller_helpers.rb
@@ -13,6 +13,6 @@ module ControllerHelpers
 end
 
 RSpec.configure do |config|
-  config.include Devise::TestHelpers, type: :controller
+  config.include Devise::Test::ControllerHelpers, type: :controller
   config.include ControllerHelpers,   type: :controller
 end
